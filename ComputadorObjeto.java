91736972712c681cddaf7388f9da6c821a9ad3fa
nayaraporto <naya08.vitoria@gmail.com>
2025-08-27 11:56:05 -0300

    Update ComputadorObjeto.java

--- ComputadorObjeto.java
+++ ComputadorObjeto.java
@@ -5,6 +5,7 @@ public class ComputadorObjeto {
 	public static void main(String[] args) {
 		
 		Computador computador1 = new Computador();
+		Computador computador2 = new Computador();
 		
 		computador1.setCor("Preto");
 		computador1.setMarca("Dell");
@@ -12,12 +13,29 @@ public class ComputadorObjeto {
 		
 		System.out.println("");
 		
+		computador2.setCor("Preto");
+		computador2.setMarca("Apple");
+		computador2.setTipo("Trabalho");
+		
+		System.out.println("");
+		
 		System.out.println(computador1.getCor());
 		System.out.println(computador1.getMarca());
 		System.out.println(computador1.getTipo());
 		
+        System.out.println("");
+		
+		System.out.println(computador2.getCor());
+		System.out.println(computador2.getMarca());
+		System.out.println(computador2.getTipo());
+		
 		computador1.exibirComputador();
+		computador1.ligar();
+		computador2.exibirComputador();
+		computador2.desligar();
 
 	}
 
 }
+
+
