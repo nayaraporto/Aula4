3f681b7207b88f56d713a78ae1de47bf0e7f4b3b
nayaraporto <naya08.vitoria@gmail.com>
2025-08-27 11:55:04 -0300

    Update CadeiraObjeto.java

--- CadeiraObjeto.java
+++ CadeiraObjeto.java
@@ -1,12 +1,10 @@
 package ExercíciosPOO;
-
-
-
 public class CadeiraObjeto {
 
 	public static void main(String[] args) {
 		
 		Cadeira cadeira1 = new Cadeira();
+		Cadeira cadeira2 = new Cadeira();
 		
 
 		cadeira1.setCor("Preta");
@@ -15,11 +13,29 @@ public class CadeiraObjeto {
 		
 		System.out.println("");
 		
+		cadeira2.setCor("Azul");
+		cadeira2.setMaterial("Couro");
+		cadeira2.setUsabilidade("Game");
+		
+		System.out.println("");
+		
 		System.out.println(cadeira1.getCor());
 		System.out.println(cadeira1.getMaterial());
 		System.out.println(cadeira1.getUsabilidade());
 		
+        System.out.println("");
+		
+		System.out.println(cadeira2.getCor());
+		System.out.println(cadeira2.getMaterial());
+		System.out.println(cadeira2.getUsabilidade());
+		
 		cadeira1.exibirCadeira();
+		cadeira1.disponivel1();
+		cadeira2.exibirCadeira();
+		cadeira2.disponivel2();
+		
+	
 	}
 
 }
+
